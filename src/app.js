f480922d0ec700fa686db00d46935afab5ea692c
Your Name <you@example.com>
2025-12-26 05:09:57 +0700

    Fix backend db connection and chat 401

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import { PLATFORM } from 'aurelia-pal';
+// IMPORT CSS DISINI AGAR TIDAK ERROR "Failed loading required CSS"
+import './assets/global.css';
 
 export class App {
   configureRouter(config, router) {
     this.router = router;
     config.title = 'Wanzofc Film';
     
-    // Matikan pushState biar URL jadi https://solif.vercel.app/#/home
-    // Ini lebih stabil untuk debug di Vercel
+    // Matikan pushState agar lebih stabil di Vercel
     config.options.pushState = false; 
     config.options.root = '/';
 
